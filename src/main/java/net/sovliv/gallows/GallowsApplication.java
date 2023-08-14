c029ce3ab55f5aaf131edc8a6c0dd05187599564
nobody <thegreatestemailname@gmail.com>
2023-08-14 14:08:17 +0300

    first commit

---
+++ src/main/java/net/sovliv/gallows/GallowsApplication.java
@@ -0,0 +1,13 @@
+package net.sovliv.gallows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallowsApplication.class, args);
+	}
+
+}
