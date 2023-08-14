3c81014bd72ed2bc91ef6478b92c11761a1be744
nobody <thegreatestemailname@gmail.com>
2023-08-14 14:49:54 +0300

    added dictionary, models and two game commands

---
+++ src/main/java/net/sovliv/gallows/model/Dictionary.java
@@ -0,0 +1,13 @@
+package net.sovliv.gallows.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Dictionary {
+    List<Word> words;
+}
